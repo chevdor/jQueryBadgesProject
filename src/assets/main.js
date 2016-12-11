6da161151e7362616167881c09ee01318a1f40ae
chevdor <chevdor@gmail.com>
2016-12-11 14:12:26 +0100

    First try

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
-
-  // your code will go here
-
+    var url = 'https://www.codeschool.com/users/419254.json';
+    $.ajax({
+        url: url,
+		dataType: 'jsonp',
+		success: function(response) {
+			var completed = response.courses.completed;
+			completed.forEach(function(course) {
+				var $elem = $('	<div class="course">\
+									<img src=" ' + course.badge + '"></img> \
+									<a href="'+course.url+'" target="_blank" class="btn btn-primary">See course</a> \
+								</div>')
+							.add('<h3>'+course.title+'</h3>')
+				$('#badges').append($elem);
+			}) 
+		}
+    });
 });
+
+
